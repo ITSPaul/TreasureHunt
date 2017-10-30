62ef41064edf2023f2daf1e2c34e042a9da0a24d
ITSPaul <powell.paul@itsligo.ie>
2017-10-30 23:00:37 +0000

    Upgraded to MonoGame 3.6 Project

--- TreasureHunt/Properties/AssemblyInfo.cs
+++ TreasureHunt/Properties/AssemblyInfo.cs
@@ -7,22 +7,20 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("TreasureHunt")]
 [assembly: AssemblyProduct("TreasureHunt")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("0e735664-8467-49a2-9a54-fe6b4d030858")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("817612f5-f432-40df-a0e8-1bdc8f731ce8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
